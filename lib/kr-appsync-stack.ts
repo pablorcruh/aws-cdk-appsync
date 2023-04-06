e761a61c983aab212c9556421f1d90d8255dc989
pablorcruh <pablorcruh@gmail.com>
2023-04-05 19:48:31 -0500

    add appsync stack

--- lib/kr-appsync-stack.ts
+++ lib/kr-appsync-stack.ts
@@ -1,16 +1,112 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { readFileSync } from "fs";
+import { ManagedPolicy, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import {
+  CfnGraphQLApi,
+  CfnGraphQLSchema,
+  CfnDataSource,
+  CfnResolver,
+} from "aws-cdk-lib/aws-appsync";
 
 export class KrAppsyncStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'KrAppsyncQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cloudWatchRole = new Role(this, "appSyncCloudWatchLogs", {
+      assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
+    });
+
+    cloudWatchRole.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSAppSyncPushToCloudWatchLogs"
+      )
+    );
+
+    const dynamoDBRole = new Role(this, "DynamoDBRole", {
+      assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
+    });
+
+    dynamoDBRole.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess")
+    );
+
+
+
+    const graphAPI = new CfnGraphQLApi(this, "graphqlApi", {
+      name: "sample-pipeline",
+      authenticationType: "API_KEY",
+      
+      logConfig: {
+        fieldLogLevel: "ALL",
+        cloudWatchLogsRoleArn: cloudWatchRole.roleArn,
+      },
+      xrayEnabled: false,
+    });
+
+    const apiSchema = new CfnGraphQLSchema(this, "GraphqlApiSchema", {
+      apiId: graphAPI.attrApiId,
+      definition: readFileSync("./lib/graphql/schema.graphql").toString(),
+    });
+
+    const contacts = Table.fromTableName(this,'KrAgentCountersResume', 'kr-agents-counters-resume');
+
+    const contactsTableDatasource: CfnDataSource = new CfnDataSource(
+      this,
+      "KrAgentCountersResumeTableDataSource",
+      {
+        apiId: graphAPI.attrApiId,
+        name: "KrAgentCountersResumeTableDataSource",
+        type: "AMAZON_DYNAMODB",
+        dynamoDbConfig: {
+          tableName: contacts.tableName,
+          awsRegion: this.region,
+        },
+        serviceRoleArn: dynamoDBRole.roleArn,
+      }
+    );
+
+
+    const getContactsByCreatorResolver: CfnResolver = new CfnResolver(
+      this,
+      "getContactsByCreatorResolver",
+      {
+        apiId: graphAPI.attrApiId,
+        typeName: "Query",
+        fieldName: "getData",
+        dataSourceName: contactsTableDatasource.name,
+        requestMappingTemplate: readFileSync(
+          "./lib/graphql/mappingTemplates/Query.listContacts.req.vtl"
+        ).toString(),
+
+        responseMappingTemplate: readFileSync(
+          "./lib/graphql/mappingTemplates/Query.listContacts.res.vtl"
+        ).toString(),
+      }
+    );
+
+    const getTestResolver: CfnResolver = new CfnResolver(
+      this,
+      "getTestResolver",
+      {
+        apiId: graphAPI.attrApiId,
+        typeName: "Query",
+        fieldName: "getTest",
+        dataSourceName: contactsTableDatasource.name,
+        requestMappingTemplate: readFileSync(
+          "./lib/graphql/mappingTemplates/Query.Count.req.vtl"
+        ).toString(),
+
+        responseMappingTemplate: readFileSync(
+          "./lib/graphql/mappingTemplates/Query.Count.res.vtl"
+        ).toString(),
+      }
+    );
+
+    getContactsByCreatorResolver.addDependsOn(apiSchema);
+    getTestResolver.addDependsOn(apiSchema);
+
   }
 }
